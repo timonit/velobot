40b62c827aed7e9e50e00870e2f35034fb6feb52
timonit <timonsus@gmail.com>
2024-06-01 16:23:56 +0500

    feature[package]: add bot framework

--- src/bot.ts
+++ src/bot.ts
@@ -1,8 +1,35 @@
 import 'reflect-metadata';
 import { MeetingRepo } from '@infra';
+import { Bot } from 'grammy';
 
-class Bot {
+class App {
   repos = {
     MeetingRepo
   };
 }
+
+const bot = new Bot(process.env.BOT_TOKEN);
+
+bot.command("start", (ctx) => ctx.reply("timon send hi!"));
+
+bot.command('timon', (ctx) => {
+  ctx.react('👍');
+});
+
+bot.on("message", (ctx) => {
+  console.log('ctx', ctx.msg);
+  ctx.reply('Автоответчик')
+});
+
+await bot.api.setMyCommands([
+  { command: "start", description: "Start the bot" },
+  { command: "help", description: "Show help text" },
+]);
+
+
+// Start the bot.
+bot.start({
+  onStart(info) {
+    console.log('Bot started')
+  }
+});
