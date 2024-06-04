794951cebd77263057436f84b26d962f45907fd7
timonit <timonsus@gmail.com>
2024-06-04 11:34:37 +0500

    feature[Public]: init form

--- src/bot.ts
+++ src/bot.ts
@@ -1,26 +1,33 @@
 import 'reflect-metadata';
 import { Bot } from 'grammy';
 import { CreateMeetingFeature } from '@features/meeting';
+import '@infra';
+
+const unnamed = 'Без названия';
 
 const bot = new Bot(process.env.BOT_TOKEN);
 
 bot.command("start", (ctx) => ctx.reply("timon send hi!"));
 
-bot.command('create_meeting', (ctx) => {
-  const f = CreateMeetingFeature.instace();
-  const meeting = f.execute({
+bot.command('create_meeting', async (ctx) => {
+  const feature = CreateMeetingFeature.instace();
+  const title = ctx.match || unnamed;
+  const author = await ctx.getAuthor();
+
+  const meeting = feature.execute({
+    title,
     meetingDate: new Date().toLocaleString(),
-    title: 'aaa',
-    creater: ctx.msg.chat.first_name ?? ctx.msg.chat.id.toString()
+    creater: author.user.id.toString()
   });
+
   ctx.reply(JSON.stringify(meeting));
 });
 
-bot.on("message::url", (ctx) => {
+bot.on("message", (ctx) => {
   console.log('\n===== msg =====');
-  console.log('ctx', ctx.msg);
+  console.log('ctx', ctx);
   console.log('===== end msg ===== \n');
-  ctx.reply('Автоответчик')
+  ctx.reply('Автоответчик');
 });
 
 
